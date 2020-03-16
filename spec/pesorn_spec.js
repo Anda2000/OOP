ac6e29367ce539a88ae281afffc5ef9fd819b7c7
Mduduzi <andapika15@gmail.com>
2020-03-16 11:35:19 +0200

    first commit

---
+++ spec/pesorn_spec.js
@@ -0,0 +1,31 @@
+let myTest = require("../src/person_src");
+
+let person = new myTest.Person("Ryan", 30, "male", [
+  "being a hardarse",
+  "agile",
+  "ssd hard drives"
+]);
+describe("testing function class", function() {
+  it("should return name", function() {
+    expect(person.name).toEqual("Ryan");
+  });
+  it("should return age", function() {
+    expect(person.age).toEqual(30);
+  });
+  it("should return gender", function() {
+    expect(person.gender).toEqual("male");
+  });
+  it("should return interests", function() {
+    expect(person.interests).toEqual([
+      "being a hardarse",
+      "agile",
+      "ssd hard drives"
+    ]);
+  });
+});
+describe("testing method which is hello", function() {
+  let greeting = myTest.hello;
+  it("should return hello", function() {
+    expect(myTest.person).toEqual(greeting);
+  });
+});
